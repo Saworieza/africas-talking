e94e5a7997c621f3e7bfe3561dc580f14243c98b
Charles Chuck <chalcchuck@gmail.com>
2015-09-26 23:02:05 +0300

    refactored subscription class to reduce clutter in the methods

--- lib/africas_talking/subscription.rb
+++ lib/africas_talking/subscription.rb
@@ -1,34 +1,57 @@
 class AfricasTalking::Subscription < AfricasTalking::Base
 	
+
+	#####POST
+	####/subscription/create
+	######PARAMS#############
+	#####----------------------
+	###1. PhoneNumber
+	###2. ShortCode
+	###3. Keyword
+	####
 	def create(phone_number, short_code, keyword)
-		validate_attributes_presence(%w(phone_number short_code, keyword))
+		validate_attributes(%w(phone_number short_code, keyword))
 		post('/subscription/create', subscription_body(phone_number, short_code, keyword))
 	end
 
+	#####POST
+	####/subscription/delete
+	######PARAMS#############
+	#####----------------------
+	###1. PhoneNumber
+	###2. ShortCode
+	###3. Keyword
+	####
 	def delete(phone_number, short_code, keyword)
-		validate_attributes_presence(%w(phone_number short_code, keyword))
+		validate_attributes(%w(phone_number short_code, keyword))
 		post('/subscription/delete', subscription_body(phone_number, short_code, keyword))
 	end
 
+	#####GET
+	####/subscription/create
+	######PARAMS#############
+	#####----------------------
+	###1. Phone_number
+	###2. ShortCode
+	###3. last_received_id
+	####	
 	def fetch(short_code, keyword, last_received_id=0)
-		validate_attributes_presence(%w(short_code, keyword))
-		url = %{/subscription?username=#{username}&shortCode=#{short_code_}&keyword=#{keyword_}&lastReceivedId=#{last_received_id_}}
-		response = get(url)
+		validate_attributes(%w(short_code, keyword))
 
+		url = %{/subscription?username=#{username}&shortCode=#{short_code}
+						 &keyword=#{keyword}&lastReceivedId=#{last_received_id}}
+		response = get(url)
 		subscriptions = JSON.parse(response)['responses'].collect{ |subscriber|
-			PremiumSubscriptionNumbers.new subscriber['phoneNumber'], subscriber['id']
+			PremiumSubscriptionNumbers.new(subscriber['phoneNumber'], subscriber['id'])
 		}
 
-		# 	return subscriptions
-		# else
-		# 	raise AfricasTalkingGatewayException, response
-		# end
+		#######Handle Errors
 	end
 
 
 private
 
-	def validate_attributes_presence(*attrs)
+	def validate_attributes(*attrs)
 		attrs.each do |attribute| 
 			raise AfricasTalkingGatewayException, "#{attribute} can not be blank!" if attribute.blank?
 		end
